17fcf2e234dedb05cad4a1674bd6da2f9cbeb1b4
Zalman <moishkugel@gmail.com>
2022-12-29 13:11:14 -0500

    commit tree d783fe9cb1e883355258824f4b510ccc5d76eaf0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(one, two, three)
+    @sides = []
+    @sides.push(one, two, three)
+  end
+
+  def kind
+    is_valid_triangle = true
+    
+    self.sides.each do |num|
+      if num >= self.sides.sum - num
+        is_valid_triangle = false
+      end
+    end
+    
+    if self.sides.include?(0)
+      is_valid_triangle = false
+    end
+
+    self.sides.each do |num|
+      if num.negative?
+        is_valid_triangle = false
+      end
+    end
+
+    if is_valid_triangle
+      if self.sides.uniq.count == 1
+        :equilateral
+      elsif self.sides.uniq.count == 2
+        :isosceles
+      elsif self.sides.uniq.count == 3
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
